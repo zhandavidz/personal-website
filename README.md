0601d3998ae6d2ff0564b47dd08fc4c9166b0121
David Zhan <zhandavidz@gmail.com>
2023-07-15 00:30:00 -0700

    add favicons

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # My Personal Website
 
 A repo for my personal website, which is currently live at [davidzhan.com](https://davidzhan.com). There are plans to upgrade this and make this look nicer in the future.
+
+## Favicons
+
+Currently favicons live in the `/favicons` folder, and the HTML tags to use them are autoloaded using `/defaults/head-tags.js`. Additionally, there is a copy of `favicon.ico` in the root folder so that browsers always have a backup (i.e. IE). **Copies of the original 512x512 logo are in the `/assets` folder.**
--- defaults/head-tags.js
+++ defaults/head-tags.js
@@ -9,6 +9,19 @@
 class HeadTags extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+            <link rel="manifest" href="/favicons/site.webmanifest">
+            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#3828ee">
+            <link rel="shortcut icon" href="/favicons/favicon.ico">
+            <meta name="msapplication-TileColor" content="#3828ee">
+            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+            <meta name="theme-color" content="#ffffff">
+            
+            
+
+
             <script src="https://kit.fontawesome.com/ccf5dd8bbb.js" crossorigin="anonymous"></script>
 
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900">
