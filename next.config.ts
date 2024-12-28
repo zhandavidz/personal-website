0314b2b2481ae61d12538d5425e8c7f6f1be664d
David Zhan <zhandavidz@gmail.com>
2024-12-27 23:48:12 -0800

    try to include static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
