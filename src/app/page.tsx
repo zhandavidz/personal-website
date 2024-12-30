6f251f54752fa1cc084625232431858f3d8d0110
David Zhan <zhandavidz@gmail.com>
2024-12-30 02:53:08 -0800

    update pfp to yosemite pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import ExportedImage from "next-image-export-optimizer";
-import testPictureStatic from "../../public/images/me.jpg";
+import testPictureStatic from "../../public/images/me.png";
 
 export default function Home() {
   return (
     <div>
-      <h1>David Zhan</h1>
+      <h1>Hi, I&apos;m David Zhan. I&apos;m a software developer.</h1>
+      <h1>Currently: Japan Intern / Undergraduate Course Assistant</h1>
       <p>David Zhan&apos;s personal website, holding his portfolio, resume, and transcript.</p>
       <ExportedImage
         src={testPictureStatic}
         alt="Picture of the author"
-        width={500}
-        height={500}
+        width={100}
+        height={100}
+        className="rounded-full"
       />
     </div>
   );
