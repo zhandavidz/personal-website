976c5b201243e9820290d8bb23c89e476af9c20e
David Zhan <zhandavidz@gmail.com>
2025-02-19 16:59:08 -0500

    force default theme to be dark

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes'
 
 export default function Theme({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider enableSystem={false} defaultTheme={"dark"} attribute="class">
       {children}
     </ThemeProvider>
   )
